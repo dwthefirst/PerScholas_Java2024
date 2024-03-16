cecf7c8a3cd19bc65ef729da6a701e43903cab66
ASUs <dwthefirst@gmail.com>
2024-03-16 14:48:57 -0700

    added files and folder for Guided Labs 303.2.1 and 303.2.2

---
+++ Module 303/GuidedLabs_303.2/src/GLAB_303_2_1/LogicalOperatorsDemo.java
@@ -0,0 +1,21 @@
+package GLAB_303_2_1;
+
+public class LogicalOperatorsDemo {
+    public static void main(String[] args) {
+        /*
+            LOGICAL OPERATORS
+         */
+        boolean x = true;
+        boolean y = false;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        System.out.println("x & y : " + (x & y));
+        System.out.println("x && y : " + (x && y));
+        System.out.println("x | y : " + (x | y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("x ^ y : " + (x ^ y));
+        System.out.println("!x : " + (!x));
+
+    }
+}
